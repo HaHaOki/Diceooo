79ee6812b91e2168ed559dc941cefffd5b98a533
Harry Havard <32362905+HaHaOki@users.noreply.github.com>
2018-05-08 09:41:19 +0100

    Changes-16

--- www/js/index.js
+++ www/js/index.js
@@ -22,17 +22,204 @@ Button on click event listener.
 Bluetooth.initiateScan;
 message.alert(Results);
 
-
-document.getElementById("BtSearch").onclick = btSearch;
-
 */
+//Bluetooth plugin initialize
+//Testing stuff goes here
+//Initialize
+document.addEventListener('deviceready', function () {
+
+    new Promise(function (resolve) {
+
+        bluetoothle.initialize(resolve, { request: true, statusReceiver: false });
+
+    }).then(initializeSuccess, handleError);
+
+});
+
+//Success
+function initializeSuccess(result) {
+
+    if (result.status === "enabled") {
+
+        log("Bluetooth is enabled.");
+        log(result);
+    }
+
+    else {
+
+        document.getElementById("start-scan").disabled = true;
+
+        log("Bluetooth is not enabled:", "status");
+        log(result, "status");
+    }
+}
+
+//Enable Log
+function log(msg, level) {
+
+    level = level || "log";
+
+    if (typeof msg === "object") {
+
+        msg = JSON.stringify(msg, null, "  ");
+    }
+
+    console.log(msg);
+
+    if (level === "status" || level === "error") {
+
+        var msgDiv = document.createElement("div");
+        msgDiv.textContent = msg;
+
+        if (level === "error") {
+
+            msgDiv.style.color = "red";
+        }
+
+        msgDiv.style.padding = "5px 0";
+        msgDiv.style.borderBottom = "rgb(192,192,192) solid 1px";
+        document.getElementById("output").appendChild(msgDiv);
+    }
+}
+
+
+//Start scanning
+var foundDevices = [];
+
+function startScan() {
+
+    log("Starting scan for devices...", "status");
+
+    foundDevices = [];
+
+    document.getElementById("devices").innerHTML = "";
+    document.getElementById("services").innerHTML = "";
+    document.getElementById("output").innerHTML = "";
+
+    if (window.cordova.platformId === "windows") {
+
+        bluetoothle.retrieveConnected(retrieveConnectedSuccess, handleError, {});
+    }
+    else {
+
+        bluetoothle.startScan(startScanSuccess, handleError, { services: [] });
+    }
+}
+
+//Scann successful
+function startScanSuccess(result) {
+
+    log("startScanSuccess(" + result.status + ")");
+
+    if (result.status === "scanStarted") {
+
+        log("Scanning for devices (will continue to scan until you select a device)...", "status");
+    }
+    else if (result.status === "scanResult") {
+
+        if (!foundDevices.some(function (device) {
+
+            return device.address === result.address;
+
+        })) {
+
+            log('FOUND DEVICE:');
+            log(result);
+            foundDevices.push(result);
+            addDevice(result.name, result.address);
+        }
+    }
+}
+
+//Windows phone function
+function retrieveConnectedSuccess(result) {
+
+    log("retrieveConnectedSuccess()");
+    log(result);
+
+    result.forEach(function (device) {
+
+        addDevice(device.name, device.address);
+
+    });
+}
+
+//Button creation
+function addDevice(name, address) {
+
+    var button = document.createElement("button");
+    button.style.width = "100%";
+    button.style.padding = "10px";
+    button.style.fontSize = "16px";
+    button.textContent = name + ": " + address;
+
+    button.addEventListener("click", function () {
+
+        document.getElementById("services").innerHTML = "";
+        connect(address);
+    });
+
+    document.getElementById("devices").appendChild(button);
+}
+
+//Connect to a device
+function connect(address) {
+
+    log('Connecting to device: ' + address + "...", "status");
+
+    if (cordova.platformId === "windows") {
+
+        getDeviceServices(address);
+
+    }
+    else {
+
+        stopScan();
+
+        new Promise(function (resolve, reject) {
+
+            bluetoothle.connect(resolve, reject, { address: address });
+
+        }).then(connectSuccess, handleError);
+
+    }
+}
+
+function stopScan() {
+
+    new Promise(function (resolve, reject) {
+
+        bluetoothle.stopScan(resolve, reject);
+
+    }).then(stopScanSuccess, handleError);
+}
+
+function stopScanSuccess() {
+
+    if (!foundDevices.length) {
+
+        log("NO DEVICES FOUND");
+    }
+    else {
+
+        log("Found " + foundDevices.length + " devices.", "status");
+    }
+}
+//Testing stuff ends here
+
+//Enable bluetooth on the device 
+bluetoothle.enable(enableSuccess, enableError);
+
 var Search = document.getElementById("BTSearch");
 var Disconnect = document.getElementById("BTDisconnect");
 
+//Scanning for devices function
 function btConnect() {
     alert("connect button tapped");
+    bluetoothle.startScan(startScanSuccess, startScanError, params);
     }
 
+//Disconnect function
     function btDisconnect() {
         alert("disconnect button tapped");
     
@@ -40,14 +227,6 @@ function btConnect() {
 
     Search.addEventListener("click", btConnect);
     Disconnect.addEventListener("click", btDisconnect);
-
-    function testConnect() {
-        alert("Connect button tapped");
-    }
-
-    function testDisconnect() {
-        alert("Disconnect button tapped");
-    }
 
 /*
 
@@ -95,6 +274,10 @@ html.picture = 6
 
  Page 3 Javascript
 */
+
+//A clever person could iterate these functions to be more efficient, however I am not a clever person.
+
+//Vibrate once function
 var one = document.getElementById("numberOne");
     function vibOne(){
         navigator.vibrate(500);
@@ -102,7 +285,7 @@ var one = document.getElementById("numberOne");
 
 one.addEventListener("click", vibOne);
 
-
+//Vibrate twice function
 var two = document.getElementById("numberTwo");
     function vibTwo(){
         navigator.vibrate([500, 500, 500]);
@@ -110,6 +293,7 @@ var two = document.getElementById("numberTwo");
 
 two.addEventListener("click", vibTwo);
 
+//Vibrate three times function
 var three = document.getElementById("numberThree");
     function vibThree(){
         navigator.vibrate([500, 500, 500, 500, 500]);
@@ -117,18 +301,21 @@ var three = document.getElementById("numberThree");
 
 three.addEventListener("click", vibThree);
 
+//Vibrate four times function
 var four = document.getElementById("numberFour");
     function vibFour(){
         navigator.vibrate([500, 500, 500, 500, 500, 500, 500])
     }
 four.addEventListener("click", vibFour);
 
+//Vibrate five times function
 var five = document.getElementById("numberFive");
     function vibFive (){
         navigator.vibrate([500, 500, 500, 500, 500, 500, 500, 500, 500])
     }
 five.addEventListener("click", vibFive);
 
+//Vibrate six times function
 var six = document.getElementById("numberSix");
     function vibSix (){
         navigator.vibrate([500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500])
